2719ad61a41efe5e0e966216789d81781cee3eb0
riyazsheikh89 <wbriyaz89@gmail.com>
2024-05-04 11:42:15 +0530

    added statistics api

--- backend/src/controller/transaction-controller.js
+++ backend/src/controller/transaction-controller.js
@@ -33,19 +33,66 @@ const getAllTransactions = async (req, res) => {
 
 const initializeDatabase = async (req, res) => {
     try {
-        // Fetch data from the third-party API
-        const response = await axios.get(TRANSACTION_API_END_POINT);
-        const products = response.data;
-        await Transaction.insertMany(products);
+      const totalDocuments = await Transaction.countDocuments();
+      if (totalDocuments > 0) {
+        return res.status(200).json({ message: "Database already initialized." });
+      }
+      // Fetch data from the third-party API
+      const response = await axios.get(TRANSACTION_API_END_POINT);
+      const products = response.data;
+      await Transaction.insertMany(products);
 
-        res.status(200).json({ message: "Database initialized successfully." });
+      res.status(200).json({ message: "Database initialized successfully." });
     } catch (error) {
-        console.error("Error initializing database:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+      console.error("Error initializing database:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
 };
+
+const getStatistics = async (req, res) => {
+  try {
+    const { month } = req.query;
+    const parsedMonth = parseInt(month);
+    if (isNaN(parsedMonth) || parsedMonth < 1 || parsedMonth > 12) {
+      return res.status(400).json({ error: 'Invalid month value. Month must be a number between 1 and 12.' });
+    }
+
+    // Define the query to filter transactions for the given month
+    const query = {
+      $expr: { $eq: [{ $month: "$dateOfSale" }, parsedMonth] }
+    };
+
+    // Calculate total sale amount for the given month
+    const totalSaleAmount = await Transaction.aggregate([
+      { $match: query },
+      { $group: { _id: null, total: { $sum: "$price" } } }
+    ]);
+
+    // Calculate total number of sold items for the given month
+    const totalSoldItems = await Transaction.countDocuments({ ...query, sold: true });
+
+    // Calculate total number of not sold items for the given month
+    const totalNotSoldItems = await Transaction.countDocuments({ ...query, sold: false });
+
+    // Return the statistics
+    res.status(200).json({
+      success: true,
+      message: "successfully calculated the statistics",
+      err: {},
+      data: {
+        totalSaleAmount: totalSaleAmount.length > 0 ? totalSaleAmount[0].total : 0,
+        totalSoldItems: totalSoldItems,
+        totalNotSoldItems: totalNotSoldItems
+      }
+    });
+  } catch (error) {
+    console.error('Error fetching statistics:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
 
 module.exports = {
   getAllTransactions,
   initializeDatabase,
+  getStatistics
 };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {getAllTransactions, initializeDatabase} = require("../controller/transaction-controller");
+const {getAllTransactions, initializeDatabase, getStatistics} = require("../controller/transaction-controller");
 
 // Route for creating a new class
 router.get("/get-all", getAllTransactions);
 router.post("/initialize", initializeDatabase);
+router.get("/statistics", getStatistics);
 
 module.exports = router;
