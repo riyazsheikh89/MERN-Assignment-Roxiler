2d6efadb9ecf14e1ac20d2269b903ce7afb62c8a
riyazsheikh89 <wbriyaz89@gmail.com>
2024-05-05 18:50:33 +0530

    updated code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require('path');
+const cors = require("cors");
 
 const { PORT } = require('./config/env-variables');
 const connectDB = require("./config/db-config");
@@ -9,11 +11,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-    res.send("<h1>Server is running perfectly 🎉 </h1>");
-});
-
 app.use("/api/v1", v1Routes);
+
+                        // DEPLOYMENT
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname1, "./src/dist")));
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "dist", "index.html"));
+  });
+} 
+else {
+  // this is to just showcase the user that backend running successfully,
+  // but, this is not inside PRODUCTION MODE
+  app.use("/*", (req, res) => {
+    res.send(`Backend is running successfuly on PORT: ${PORT}. Warning! This is a not inside PRODUCTION MODE`);
+  });
+}
+                        // DEPLOYMENT
 
 
 app.listen(PORT, async () => {
